6cd8489187604934e44f6284bdac3d7b275c3b86
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-25 12:02:03 +0600

    style addad & display food in UI

--- src/component/Meal/Meal.js
+++ src/component/Meal/Meal.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Meal.css';
 
 const Meal = (props) => {
-    console.log(props.meal);
+    //console.log(props.meal);
+    const { strMeal, strCategory, price, strMealThumb, strInstructions } = props.meal
+
     return (
-        <div>
-            <h2>Single Meal</h2>
+        <div className="meal-box-style">
+            <div>
+                <img className="meal-Picture-style" src={strMealThumb} alt="" sizes="" srcset="" />
+            </div>
+            <div className="meal-detals">
+                <h3> Name : {strMeal}</h3>
+                <h5>Category : {strCategory}</h5>
+                <p>{strInstructions.slice(0, 100)}</p>
+                <h5>Price : {price}</h5>
+                <button>Add To Card</button>
+                <button>Detals</button>
+            </div>
+            {/* <div className="btn-section">
+                <button>Add To Card</button>
+                <button>Detals</button>
+            </div> */}
         </div>
     );
 };
--- src/component/Meals/Meals.js
+++ src/component/Meals/Meals.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Meal from '../Meal/Meal';
+import './Meals.css'
 
 const Meals = () => {
     const [meals, setMeals] = useState([]);
     useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+        fetch('./utilytis/fackData.JSON')
             .then(res => res.json())
             .then(data => setMeals(data))
     }, [])
 
+
+
     return (
         <div>
             <h2>Meal DB</h2>
-            <div>
+            <div className="grid-style">
+
                 {
                     meals.map(meal => <Meal meal={meal}></Meal>)
                 }
